b790f44294ec5bc2e96d93b9b15aed8d0ac2dd45
eJwtzLFuwjAUheF38ZzBpTEk3pAISK3YWlbLvrkOTkxsXSeFCPHutVC2oyN9/5NpAEzpJww4MsmwKgWWnIM1G1ObrS2x4ry29QevrBafOy3AoAVWsEjhz7VIK32uJTWtqe4aFGzT7wX1Tojef6XmSOezE0PHm8P+u7+Up6aHJafeRE1LxOwMakLKb4LwPnR7c6OMs/EO1IBLQahbGajLI4ZiTkgSb9r54h5osD7cM8ZHdIRJ6YnJcfb+9foHstpQgg== <axadovravshan@mail.ru>
2023-01-23 09:34:19 +0500

    Add project files.

---
+++ Daily LeetCode/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Daily_LeetCode;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] arr =  new int[]{ 0, 1,2,3,4,5 };
+        Console.WriteLine(string.Join(",",solution.Shuffle(arr, arr.Length / 2 )));
+    }
+}
