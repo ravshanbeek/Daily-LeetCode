ea78e9ef7c60c0732e609eb78640766ebfd60233
eJwtzLFuwjAUheF38ZzBpTEk3pAISK3YWlbLvrkOTkxsXSeFCPHutVC2oyN9/5NpAEzpJww4MsmwKgWWnIM1G1ObrS2x4ry29QevrBafOy3AoAVWsEjhz7VIK32uJTWtqe4aFGzT7wX1Tojef6XmSOezE0PHm8P+u7+Up6aHJafeRE1LxOwMakLKb4LwPnR7c6OMs/EO1IBLQahbGajLI4ZiTkgSb9r54h5osD7cM8ZHdIRJ6YnJcfb+9foHstpQgg== <axadovravshan@mail.ru>
2023-01-27 23:04:11 +0500

    IsStrictlyPalindromic is solved

--- Daily LeetCode/Program.cs
+++ Daily LeetCode/Program.cs
@@ -3,6 +3,6 @@
     static void Main(string[] args)
     {
         Solution solution = new Solution();
-        Console.WriteLine(solution.MinimumSum(2932));
+        Console.WriteLine (string.Join(",", solution.PivotArray(new int[] {9,12,5,10,13,0,3,10,15,16,17},10)));
     }
 }
