2e2fe28091fb27a46c81be1faf64bfd5d7538cc9
eJwtzLFuwjAUheF38ZzBpTEk3pAISK3YWlbLvrkOTkxsXSeFCPHutVC2oyN9/5NpAEzpJww4MsmwKgWWnIM1G1ObrS2x4ry29QevrBafOy3AoAVWsEjhz7VIK32uJTWtqe4aFGzT7wX1Tojef6XmSOezE0PHm8P+u7+Up6aHJafeRE1LxOwMakLKb4LwPnR7c6OMs/EO1IBLQahbGajLI4ZiTkgSb9r54h5osD7cM8ZHdIRJ6YnJcfb+9foHstpQgg== <axadovravshan@mail.ru>
2023-02-15 18:05:07 +0500

    solved daily problem

--- Daily LeetCode/Program.cs
+++ Daily LeetCode/Program.cs
@@ -1,12 +1,36 @@
-﻿class Program
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace WriteTextOnImageExample
 {
-    static void Main(string[] args)
+    class Program
     {
-        int[] arr = new int[]{3, 4, -1, 1 };
-        Solution solution = new Solution();
+        private static string TableBuilder(List<string> challengers)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine(String.Format("|{0,29}", " ") + "REPORT OF TODAY");
 
-        Console.WriteLine(solution.FirstMissingPositive(arr));
+            builder.AppendLine($"<pre>|{new string('-', 27)}|{new string('-', 8)}|{new string('-', 8)}|{new string('-', 13)}|");
+
+            builder.AppendLine(String.Format("| {0, -25} | {1, -6} | {2, -6} | {3, -13}|",
+                "UserName", "Heart", "Today", "TotalSolved"));
+
+            builder.AppendLine($"|{new string('-', 27)}|{new string('-', 8)}|{new string('-', 8)}|{new string('-', 13)}|");
+
+            foreach (var challanger in challengers)
+            {
+                builder.AppendLine(String.Format("| {0, -25} | {1, -6} | {2, -6} | {3, -13}|",
+                    challanger, 3, 12, 214));
+            }
+
+            return builder.ToString() + "</pre>";
+        }
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.IsIsomorphic("egsd","adfd"));
+        }
     }
-
-    
 }
